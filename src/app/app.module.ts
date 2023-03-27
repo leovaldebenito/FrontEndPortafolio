783259b2dbad7a3d13ff243da78e33b7b3f34393
leovaldebenito <leonardovaldebenito21@gmail.com>
2023-03-27 00:00:15 -0300

    nav y banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { AptitudesComponent } from './componentes/aptitudes/aptitudes.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { ExperienciaYEducacionComponent } from './componentes/experiencia-y-educacion/experiencia-y-educacion.component';
+import { LogrosComponent } from './componentes/logros/logros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcercaDeComponent,
+    AptitudesComponent,
+    EncabezadoComponent,
+    ExperienciaYEducacionComponent,
+    LogrosComponent
   ],
   imports: [
     BrowserModule
